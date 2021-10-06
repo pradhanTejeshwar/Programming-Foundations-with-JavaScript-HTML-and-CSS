9ab2b5fb6d1e09479ef7f3b5333da0b48a12b72b
Tejeshwar Pradhan <t1999pradhan@gmail.com>
2021-10-06 19:51:57 +0530

    Delete js comment.js

--- Week 2/js comment.js
+++
@@ -1,7 +0,0 @@
-/*
-Import the SimpleImage JS Library from the below url :
-https://www.dukelearntoprogram.com//course1/common/js/cs101/SimpleImage.js
-
-You can also go to the below mentioned url to run these codes directly :
-https://www.dukelearntoprogram.com/course1/example/index.php
-*/
