74e171805d2ffc130b0d767d4f09fd1bdf7b519f
Tejeshwar Pradhan <t1999pradhan@gmail.com>
2021-10-06 19:51:46 +0530

    Updating codes for Week 2
    Updating some codes for Week 2 files and also adding few more New files

--- Week 2/Implementing the Green Screen Algorithm/Programming Exercise Advanced Modifying Images.js
+++ Week 2/Implementing the Green Screen Algorithm/Programming Exercise Advanced Modifying Images.js
@@ -26,3 +26,54 @@ print(output);
 
 
 //Part 2
+var img = new SimpleImage(200,200);
+for (var px of img.values()){
+  var x = px.getX();
+  var y = px.getY();
+  if (x < img.getWidth()/2) {
+    px.setRed(255);
+  }
+  if (y > img.getHeight()/2) {
+    px.setBlue(255);
+  }
+  if(!(x < img.getWidth()/2) && !(y > img.getHeight()/2)) {
+      px.setRed(0);
+      px.setGreen(255);
+      px.setBlue(0);
+  }
+//   if(x >= img.getWidth()/2 && y <= img.getHeight()/2) {
+//       px.setRed(0);
+//       px.setGreen(255);
+//       px.setBlue(0);
+//   }
+}
+print (img);
+
+
+
+//Part 3
+function setBlack(pixel) {
+    pixel.setRed(0);
+    pixel.setGreen(0);
+    pixel.setBlue(0);
+    return pixel;
+}
+
+function addBorder(image, length) {
+    for(var pixels of image.values()) {
+        var x = pixels.getX();
+        var y = pixels.getY();
+        width = image.getWidth();
+        height = image.getHeight();
+        if((pixels.getX() < length) || (pixels.getY() < length)) {
+            setBlack(pixels);
+        }
+        if((pixels.getX() > (width - length)) || (pixels.getY() > (height -length))) {
+            setBlack(pixels);
+        }
+    }
+    return image;
+}
+
+var picture = new SimpleImage("skyline.jpg");
+print(addBorder(picture,10));
