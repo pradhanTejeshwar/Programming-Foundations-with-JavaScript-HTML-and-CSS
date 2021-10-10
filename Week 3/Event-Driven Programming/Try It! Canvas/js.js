0a24c300f163de0826f746825bb7d0ca85b60bdb
Tejeshwar Pradhan <t1999pradhan@gmail.com>
2021-10-10 13:04:46 +0530

    Updated Changes

--- Week 3/Event-Driven Programming/Try It! Canvas/js.js
+++ Week 3/Event-Driven Programming/Try It! Canvas/js.js
@@ -1,14 +1,6 @@
-function displayAlert() {
-	alert('Alert message to be displayed Here.');
-}
-
-function something() {
-	var choice = confirm('Select anyone');
-	if(choice==true) {
-		var message = 'You pressed OK!';
-	}
-	else {
-		var message = 'Are you sure you want to cancel?';
-	}
-	alert(message);
+function changeColor() {
+	var divElement1 = document.getElementById('div1');
+	var divElement2 = document.getElementById('div2');
+	divElement1.className = "blueback";
+	divElement2.className = "orangeback";
 }
