6444f90d470b073fb987e0fca07b5b2ef5f6719f
Tejeshwar Pradhan <t1999pradhan@gmail.com>
2021-10-10 13:34:08 +0530

    Final Updated File Contents

--- Week 3/Event-Driven Programming/Try It! Canvas/js.js
+++ Week 3/Event-Driven Programming/Try It! Canvas/js.js
@@ -4,3 +4,45 @@ function changeColor() {
 	divElement1.className = "blueback";
 	divElement2.className = "orangeback";
 }
+
+function changeText() {
+	var divElement1 = document.getElementById('div1');
+	var divElement2 = document.getElementById('div2');
+	divElement1.innerHTML = "Erste";
+	divElement2.innerHTML = "Zweite";
+}
+
+function changeStyleColor() {
+	var divElement1 = document.getElementById('div1');
+	var divElement2 = document.getElementById('div2');
+	divElement1.style.color = "magenta";
+	divElement2.style.color = "lightblue";
+}
+
+function displayValue() {
+	var divElement1 = document.getElementById('button4');
+	alert(divElement1.value);
+}
+
+function changeValue() {
+	var divElement1 = document.getElementById('button4');
+	divElement1.value = "Hello";
+}
+
+function doBlue() {
+	var divElement1 = document.getElementById('div1');
+	divElement1.style.backgroundColor = "blue";
+	var objL = divElement1.getContext("2d");
+	objL.fillStyle = "yellow";
+	objL.fillRect(10 , 10 , 80 , 80);
+	objL.fillStyle = "black";
+	objL.font = "20px Arial";
+	objL.fillText("Hello" , 25 , 50);
+}
+
+function doOrange() {
+	var divElement1 = document.getElementById("div1");
+  	divElement1.style.backgroundColor = "orange";
+  	var objL = divElement1.getContext("2d");
+  	objL.clearRect(10 , 10 , 80 , 80);
+}
